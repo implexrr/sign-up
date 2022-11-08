34bc3dc462964952983fea161b015d01b4a1e6e5
Chenhao Wang <implexrr@gmail.com>
2022-11-07 23:12:14 -0500

    Add input validation

--- js-files/index.js
+++ js-files/index.js
@@ -1,5 +1,38 @@
 let inputs = document.querySelectorAll("input");
 for (let i = 0; i < inputs.length; i++) {
-  inputs[i].style.backgroundColor = "red";
+  if (inputs[i].required == true && inputs[i].type != "password") {
+    inputs[i].addEventListener("input", check);
+  }
+}
+function check (e) {
+  console.log(e.target.value);
+  if (e.target.value != "") {
+    e.target.classList.add("confirmation");
+  }
+  else {
+    e.target.classList.remove("confirmation");
+  }
 }
 
+let password = document.querySelector("#password");
+let confirmation = document.querySelector("#confirmation");
+let create = document.querySelector("#create");
+let form = document.querySelector("form")
+form.addEventListener("submit", confirm);
+function confirm (e) {
+  if (password.value != confirmation.value) {
+    e.preventDefault();
+    setTimeout((() => {
+      create.classList.add("shake");
+      create.textContent = "Passwords must match";
+      }), 0);
+
+    setTimeout((() => {
+      create.classList.remove("shake");
+      create.textContent = "Create Account";
+      }), 300);
+    return false;
+  }
+  
+  return true;
+}
